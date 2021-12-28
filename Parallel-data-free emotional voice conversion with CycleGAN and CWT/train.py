cdf45ebc41004a311915f950a39a354f487aeded
ZPDoCF <42126039+CZFuChason@users.noreply.github.com>
2021-12-28 12:34:47 +0900

    Update train.py

--- Parallel-data-free emotional voice conversion with CycleGAN and CWT/train.py
+++ Parallel-data-free emotional voice conversion with CycleGAN and CWT/train.py
@@ -3,25 +3,32 @@ import numpy as np
 import argparse
 import time
 import librosa
-
+import soundfile as sf
 from preprocess import *
 from model import CycleGAN
+import tensorflow as tf
+import pyworld as pw
 
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.7)
+sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True 
+sess = tf.Session(config=config)
 
-def train(train_A_dir, train_B_dir, model_dir, model_name, random_seed, validation_A_dir, validation_B_dir, output_dir, tensorboard_log_dir):
+def train(train_A_dir, train_B_dir, model_dir, model_name, random_seed, validation_A_dir, validation_B_dir, output_dir, tensorboard_log_dir, n_frames):
 
     np.random.seed(random_seed)
 
-    num_epochs = 5000
+    num_epochs = 500
     mini_batch_size = 1 # mini_batch_size = 1 is better
     generator_learning_rate = 0.0002
-    generator_learning_rate_decay = generator_learning_rate / 200000
+    generator_learning_rate_decay = generator_learning_rate / 5000000
     discriminator_learning_rate = 0.0001
-    discriminator_learning_rate_decay = discriminator_learning_rate / 200000
-    sampling_rate = 16000
+    discriminator_learning_rate_decay = discriminator_learning_rate / 5000000
+    sampling_rate = 24000
     num_mcep = 24
     frame_period = 5.0
-    n_frames = 128
+#     n_frames = 128
     lambda_cycle = 10
     lambda_identity = 5
 
@@ -57,12 +64,12 @@ def train(train_A_dir, train_B_dir, model_dir, model_name, random_seed, validati
     np.savez(os.path.join(model_dir, 'mcep_normalization.npz'), mean_A = coded_sps_A_mean, std_A = coded_sps_A_std, mean_B = coded_sps_B_mean, std_B = coded_sps_B_std)
 
     if validation_A_dir is not None:
-        validation_A_output_dir = os.path.join(output_dir, 'converted_A')
+        validation_A_output_dir = os.path.join(output_dir, 'converted_A'+str(n_frames))
         if not os.path.exists(validation_A_output_dir):
             os.makedirs(validation_A_output_dir)
 
     if validation_B_dir is not None:
-        validation_B_output_dir = os.path.join(output_dir, 'converted_B')
+        validation_B_output_dir = os.path.join(output_dir, 'converted_B'+str(n_frames))
         if not os.path.exists(validation_B_output_dir):
             os.makedirs(validation_B_output_dir)
 
@@ -73,17 +80,12 @@ def train(train_A_dir, train_B_dir, model_dir, model_name, random_seed, validati
 
     print('Time Elapsed for Data Preprocessing: %02d:%02d:%02d' % (time_elapsed // 3600, (time_elapsed % 3600 // 60), (time_elapsed % 60 // 1)))
 
-    model = CycleGAN(num_features = num_mcep)
+    model = CycleGAN(num_features = num_mcep, log_dir = tensorboard_log_dir+str(n_frames))
+    if n_frames!=128:
+        model.load(model_dir+model_name)
 
     for epoch in range(num_epochs):
         print('Epoch: %d' % epoch)
-        '''
-        if epoch > 60:
-            lambda_identity = 0
-        if epoch > 1250:
-            generator_learning_rate = max(0, generator_learning_rate - 0.0000002)
-            discriminator_learning_rate = max(0, discriminator_learning_rate - 0.0000001)
-        '''
 
         start_time_epoch = time.time()
 
@@ -95,22 +97,23 @@ def train(train_A_dir, train_B_dir, model_dir, model_name, random_seed, validati
 
             num_iterations = n_samples // mini_batch_size * epoch + i
 
-            if num_iterations > 10000:
-                lambda_identity = 0
-            if num_iterations > 200000:
-                generator_learning_rate = max(0, generator_learning_rate - generator_learning_rate_decay)
-                discriminator_learning_rate = max(0, discriminator_learning_rate - discriminator_learning_rate_decay)
+            if num_iterations > 100000:
+#                 lambda_identity = 0
+                lambda_identity = 0.5
+            if num_iterations > 100000:
+                generator_learning_rate = max(0.00001, generator_learning_rate - generator_learning_rate_decay)
+                discriminator_learning_rate = max(0.00001, discriminator_learning_rate - discriminator_learning_rate_decay)
 
             start = i * mini_batch_size
             end = (i + 1) * mini_batch_size
 
             generator_loss, discriminator_loss = model.train(input_A = dataset_A[start:end], input_B = dataset_B[start:end], lambda_cycle = lambda_cycle, lambda_identity = lambda_identity, generator_learning_rate = generator_learning_rate, discriminator_learning_rate = discriminator_learning_rate)
 
-            if i % 50 == 0:
+            if i % 200 == 0:
                 #print('Iteration: %d, Generator Loss : %f, Discriminator Loss : %f' % (num_iterations, generator_loss, discriminator_loss))
-                print('Iteration: {:07d}, Generator Learning Rate: {:.7f}, Discriminator Learning Rate: {:.7f}, Generator Loss : {:.3f}, Discriminator Loss : {:.3f}'.format(num_iterations, generator_learning_rate, discriminator_learning_rate, generator_loss, discriminator_loss))
+                print('Iteration: {:07d}, Generator Learning Rate: {:.7f}, Discriminator Learning Rate: {:.7f}, Generator Loss : {:.6f}, Discriminator Loss : {:.6f}'.format(num_iterations, generator_learning_rate, discriminator_learning_rate, generator_loss, discriminator_loss))
 
-        model.save(directory = model_dir, filename = model_name)
+        model.save(directory = model_dir, filename = str(n_frames)+model_name)
 
         end_time_epoch = time.time()
         time_elapsed_epoch = end_time_epoch - start_time_epoch
@@ -118,7 +121,7 @@ def train(train_A_dir, train_B_dir, model_dir, model_name, random_seed, validati
         print('Time Elapsed for This Epoch: %02d:%02d:%02d' % (time_elapsed_epoch // 3600, (time_elapsed_epoch % 3600 // 60), (time_elapsed_epoch % 60 // 1)))
 
         if validation_A_dir is not None:
-            if epoch % 50 == 0:
+            if epoch % 5 == 0:
                 print('Generating Validation Data B from A...')
                 for file in os.listdir(validation_A_dir):
                     filepath = os.path.join(validation_A_dir, file)
@@ -135,10 +138,14 @@ def train(train_A_dir, train_B_dir, model_dir, model_name, random_seed, validati
                     coded_sp_converted = np.ascontiguousarray(coded_sp_converted)
                     decoded_sp_converted = world_decode_spectral_envelop(coded_sp = coded_sp_converted, fs = sampling_rate)
                     wav_transformed = world_speech_synthesis(f0 = f0_converted, decoded_sp = decoded_sp_converted, ap = ap, fs = sampling_rate, frame_period = frame_period)
-                    librosa.output.write_wav(os.path.join(validation_A_output_dir, os.path.basename(file)), wav_transformed, sampling_rate)
-
+                    wav_transformed = np.nan_to_num(wav_transformed)
+#                     librosa.output.write_wav(os.path.join(validation_A_output_dir, str(epoch)+'_'+os.path.basename(file)), wav_transformed, sampling_rate)
+#                     sf.write(os.path.join(validation_A_output_dir, str(epoch)+'_'+os.path.basename(file)), wav_transformed, sampling_rate)
+                    file_name = file.split('.')[0]+'.wav'
+                    sf.write(os.path.join(validation_A_output_dir, os.path.basename(file_name)), wav_transformed, sampling_rate)
+                        
         if validation_B_dir is not None:
-            if epoch % 50 == 0:
+            if epoch % 5 == 0:
                 print('Generating Validation Data A from B...')
                 for file in os.listdir(validation_B_dir):
                     filepath = os.path.join(validation_B_dir, file)
@@ -155,8 +162,12 @@ def train(train_A_dir, train_B_dir, model_dir, model_name, random_seed, validati
                     coded_sp_converted = np.ascontiguousarray(coded_sp_converted)
                     decoded_sp_converted = world_decode_spectral_envelop(coded_sp = coded_sp_converted, fs = sampling_rate)
                     wav_transformed = world_speech_synthesis(f0 = f0_converted, decoded_sp = decoded_sp_converted, ap = ap, fs = sampling_rate, frame_period = frame_period)
-                    librosa.output.write_wav(os.path.join(validation_B_output_dir, os.path.basename(file)), wav_transformed, sampling_rate)
+                    wav_transformed = np.nan_to_num(wav_transformed)
+#                     librosa.output.write_wav(os.path.join(validation_B_output_dir, str(epoch)+'_'+os.path.basename(file)), wav_transformed, sampling_rate)
+#                     sf.write(os.path.join(validation_B_output_dir, str(epoch)+'_'+os.path.basename(file)), wav_transformed, sampling_rate)
+                    sf.write(os.path.join(validation_B_output_dir, os.path.basename(file)), wav_transformed, sampling_rate)
 
+    model.save(directory = model_dir, filename = model_name)
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description = 'Train CycleGAN model for datasets.')
@@ -170,7 +181,9 @@ if __name__ == '__main__':
     validation_B_dir_default = './data/evaluation_all/SURPRISE'
     output_dir_default = './validation_output'
     tensorboard_log_dir_default = './log'
-
+    
+    
+    parser.add_argument('--num_f', type = int, help = 'frame length.')
     parser.add_argument('--train_A_dir', type = str, help = 'Directory for A.', default = train_A_dir_default)
     parser.add_argument('--train_B_dir', type = str, help = 'Directory for B.', default = train_B_dir_default)
     parser.add_argument('--model_dir', type = str, help = 'Directory for saving models.', default = model_dir_default)
@@ -183,9 +196,12 @@ if __name__ == '__main__':
 
     argv = parser.parse_args()
 
+    num_f = argv.num_f
+    print('frame length:',num_f)
     train_A_dir = argv.train_A_dir
     train_B_dir = argv.train_B_dir
     model_dir = argv.model_dir
+#     model_name = str(num_f)+'_'+argv.model_name
     model_name = argv.model_name
     random_seed = argv.random_seed
     validation_A_dir = None if argv.validation_A_dir == 'None' or argv.validation_A_dir == 'none' else argv.validation_A_dir
@@ -193,4 +209,4 @@ if __name__ == '__main__':
     output_dir = argv.output_dir
     tensorboard_log_dir = argv.tensorboard_log_dir
 
-    train(train_A_dir = train_A_dir, train_B_dir = train_B_dir, model_dir = model_dir, model_name = model_name, random_seed = random_seed, validation_A_dir = validation_A_dir, validation_B_dir = validation_B_dir, output_dir = output_dir, tensorboard_log_dir = tensorboard_log_dir)
+    train(train_A_dir = train_A_dir, train_B_dir = train_B_dir, model_dir = model_dir, model_name = model_name, random_seed = random_seed, validation_A_dir = validation_A_dir, validation_B_dir = validation_B_dir, output_dir = output_dir, tensorboard_log_dir = tensorboard_log_dir, n_frames = num_f)
